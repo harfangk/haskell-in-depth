aa8987221d1b83f0d858907f075f8c1fe40bd7c6
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2023-08-08 01:11:02 +0900

    Chapter 2
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

---
+++ ch02/person/implemented-show.hs
@@ -0,0 +1,10 @@
+import Person
+
+instance Show Person where
+  show (Person name Nothing) = name
+  show (Person name (Just age)) = name ++ " (" ++ show age ++ ")"
+
+main :: IO ()
+main = do
+  print homer
+  print spj
---
+++ ch02/person/text-show.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Person
+import TextShow
+
+instance TextShow Person where
+  showb (Person name Nothing) = fromString name
+  showb (Person name (Just age)) = fromString name <> " (" <> showb age <> ")"
+
+main :: IO ()
+main = do
+  printT homer
+  printT spj
